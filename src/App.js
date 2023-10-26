f3fa0ee96c00c8cfcbc1d9dd2d1a1f159a4f7e95
alice4599 <alice45992000@gmail.com>
2023-10-26 17:13:30 +0200

    added dictionary component with search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <div className="openSource">
+            <a
+              className="source"
+              href="https://github.com/alice4599/dictionary-app.git"
+              target="_blank"
+            >
+              Open-source code
+            </a>
+            by Neli
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
