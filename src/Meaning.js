2d304ccb7683592ace96ecd6ea28a503d4332b5b
alice4599 <alice45992000@gmail.com>
2023-10-28 22:43:33 +0200

    added synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning.synonyms);
   return (
     <div>
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>{props.meaning.definition}</p>
       <p>{props.meaning.example}</p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
