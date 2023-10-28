df09749d3eb036b93ea2797b7c9997219f115a23
alice4599 <alice45992000@gmail.com>
2023-10-28 11:55:28 +0200

    added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function displayResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}`);
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=4dad04c3ca4f2774bb04900t8b93bo1f`;
+    axios.get(apiUrl).then(displayResponse);
   }
   function handleKeywordSearch(event) {
     setKeyword(event.target.value);
