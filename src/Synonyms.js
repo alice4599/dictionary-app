1f9ac1364455fc261b02801f587252de10d048ca
alice4599 <alice45992000@gmail.com>
2023-10-31 11:54:07 +0100

    updated styling

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,7 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <div>
-        <strong>Synonyms</strong>
+        <strong>Synonyms:</strong>
         <ul className="Synonyms">
           {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
